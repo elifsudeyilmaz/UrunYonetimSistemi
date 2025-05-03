1755667aefd7bd4b7b8a072bca5e4f1afa7401cf
silaacis <161122870+silaacis@users.noreply.github.com>
2025-05-03 17:05:45 +0300

    Product sınıfı ve LinkedList yapısı eklendi

---
+++ UrunYonetimSistem/DenemeUrunYonetim/ProductLinkedList.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DenemeUrunYonetim
+{
+    // ProductLinkedList.cs
+    // Ürünleri bağlı liste (LinkedList) şeklinde tutar ve yönetir
+
+    public class ProductLinkedList
+    {
+        private ProductNode head;
+
+        public ProductLinkedList()
+        {
+            head = null;
+        }
+
+        // Ürün ekleme
+        public void AddProduct(Product product)
+        {
+            ProductNode newNode = new ProductNode(product);
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                ProductNode current = head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+        }
+
+        // Ürünleri listeleme
+        public void DisplayProducts()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Ürün listesi boş.");
+                return;
+            }
+
+            ProductNode current = head;
+            while (current != null)
+            {
+                Console.WriteLine(current.Data.ToString());
+                current = current.Next;
+            }
+        }
+    }
+
+}
---
+++ UrunYonetimSistem/DenemeUrunYonetim/ProductNode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DenemeUrunYonetim
+{
+    // ProductNode.cs
+    // LinkedList içinde kullanılacak ürün düğümü (Node)
+
+    public class ProductNode
+    {
+        public Product Data { get; set; }
+        public ProductNode Next { get; set; }
+
+        public ProductNode(Product data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+
+}
